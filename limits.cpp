a8f6814a3cdefd6f785b87ba198bba8fb258d532
Philuo <fc1532397@2980.com>
2019-09-14 12:05:36 +0800

    pitch for testCases of limits

--- limits.cpp
+++ limits.cpp
@@ -5,15 +5,24 @@ using namespace std;
 
 int main()
 {
-    cout << INT_MIN << endl;
-    cout << INT_MAX << endl;
+    cout << INT_MIN << endl; //-2147483648
+    cout << INT_MAX << endl; //2147483647
 
-    cout << CHAR_MIN << endl;
-    cout << CHAR_MAX << endl;
+    cout << CHAR_MIN << endl; //-128
+    cout << CHAR_MAX << endl; //127
 
-    cout << numeric_limits<int>::min() << endl;
-    cout << numeric_limits<int>::max() << endl;
+    cout << numeric_limits<int>::min() << endl; //-2147483648
+    cout << numeric_limits<int>::max() << endl; //2147483647
 
-    cout << 0x7fffffff << endl;
-    cout << 0xffffffff << endl;
+    cout << numeric_limits<unsigned>::min() << endl; //0
+    cout << numeric_limits<unsigned>::max() << endl; //4294967295
+
+    cout << 0x7fffffff << endl; //2147483647
+    cout << 0x80000000 << endl; //-2147483648(int) 2147483648(unsigned)
+    cout << 0xffffffff << endl; //4294967295(unsigned) -2147483647(int)
+
+    cout << "Program default print unsigned decimal" << endl;
+    cout << "Program Special Processing of Boundary Values of Signed Numbers" << endl;
+    cout << "view -0 as -2147483648 in signed numer" << endl;
+    cout << "view -0 as 2147483648 in unsigned numer" << endl;
 }
